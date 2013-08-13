d22d827f04ece8999ef545ff25eb4aa6a1e6e152
tako0910 <tako.oct0910@gmail.com>
2013-08-13 13:59:49 +0900

    Update mountpoint

--- rootdir/etc/extsdperm.sh
+++ rootdir/etc/extsdperm.sh
@@ -1,2 +1,2 @@
-busybox umount /storage/sdcard1
-busybox mount -t auto -o dirsync,nosuid,nodev,noexec,relatime,uid=1000,gid=1015,fmask=0702,dmask=0702 /dev/block/mmcblk1p1 /storage/sdcard1
+busybox umount /storage/sdcard
+busybox mount -t auto -o dirsync,nosuid,nodev,noexec,relatime,uid=1000,gid=1015,fmask=0702,dmask=0702 /dev/block/mmcblk1p1 /storage/sdcard
