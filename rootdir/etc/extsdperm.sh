bc7fc9e341b8ec25b392b9fa7b359e456133a40b
tako0910 <tako.oct0910@gmail.com>
2013-08-13 14:49:32 +0900

    fix ext_sd mounter

--- rootdir/etc/extsdperm.sh
+++ rootdir/etc/extsdperm.sh
@@ -1,2 +1,2 @@
-busybox umount /storage/sdcard
-busybox mount -t auto -o dirsync,nosuid,nodev,noexec,relatime,uid=1000,gid=1015,fmask=0702,dmask=0702 /dev/block/mmcblk1p1 /storage/sdcard
+busybox umount /storage/ext_sd
+busybox mount -t auto -o dirsync,nosuid,nodev,noexec,relatime,uid=1000,gid=1015,fmask=0702,dmask=0702 /dev/block/mmcblk1p1 /storage/ext_sd
