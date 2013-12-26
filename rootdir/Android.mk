8a0327618014c48f7d7807d53ff8d009069d5eb3
Steve Kondik <shade@chemlab.org>
2013-12-25 18:46:22 -0800

    m7-common: Remove modem links, generate firmware symlinks
    * Because remounting the system partition to symlink firmware is dumb.
    
    Change-Id: I55b0ad232fb5dbb1abc57bcb2ace66be1737e7ca
    
    Conflicts:
    
    	m7-common.mk
    	rootdir/Android.mk
    	rootdir/etc/remount.qcom.cdma
    
    Conflicts:
    
    	Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter m7att m7tmo m7ul,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/fstab.qcom.m7wlj
+LOCAL_SRC_FILES		:= etc/fstab.qcom.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -12,25 +13,64 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.m7wlj
+LOCAL_SRC_FILES		:= etc/init.target.rc.gsm
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(TARGET_DEVICE),m7vzw)
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/fstab.qcom.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= remount.qcom
+LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/remount.qcom.m7wlj
+LOCAL_SRC_FILES		:= etc/init.target.rc.vzw
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(TARGET_DEVICE),m7spr)
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/fstab.qcom.spr
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.firmware_links.sh
+LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qcom.firmware_links.sh
+LOCAL_SRC_FILES		:= etc/init.target.rc.spr
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(TARGET_DEVICE),m7wlj)
+include $(CLEAR_VARS)
+LOCAL_MODULE                := fstab.qcom
+LOCAL_MODULE_TAGS        := optional eng
+LOCAL_MODULE_CLASS        := ETC
+LOCAL_SRC_FILES                := etc/fstab.qcom.m7wlj
+LOCAL_MODULE_PATH        := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE                := init.target.rc
+LOCAL_MODULE_TAGS        := optional eng
+LOCAL_MODULE_CLASS        := ETC
+LOCAL_SRC_FILES                := etc/init.target.rc.m7wlj
+LOCAL_MODULE_PATH        := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.rc
@@ -57,10 +97,11 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.qcom.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE		:= init.recovery.qcom.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
 
