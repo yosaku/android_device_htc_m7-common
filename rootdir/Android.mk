ec4a0a25bc71d38203d9e10e8bacb59d1e6f1d55
tako0910 <tako.oct0910@gmail.com>
2014-05-06 15:14:29 +0900

    m7wlj: use common fstab

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,19 +40,11 @@ endif
 
 ifeq ($(TARGET_DEVICE),m7wlj)
 include $(CLEAR_VARS)
-LOCAL_MODULE                := fstab.qcom
-LOCAL_MODULE_TAGS        := optional eng
-LOCAL_MODULE_CLASS        := ETC
-LOCAL_SRC_FILES                := etc/fstab.qcom.m7wlj
-LOCAL_MODULE_PATH        := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE                := init.target.rc
-LOCAL_MODULE_TAGS        := optional eng
-LOCAL_MODULE_CLASS        := ETC
-LOCAL_SRC_FILES                := etc/init.target.rc.m7wlj
-LOCAL_MODULE_PATH        := $(TARGET_ROOT_OUT)
+LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.target.rc.m7wlj
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 endif
 
